aa6fa40ff1a475f0b3e212781e748a1db522cb46
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-21 14:25:01 -0700

    working on setting my project up

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,19 @@
+
+const router = require('express').Router()
+
+
+router.use('*', (req,res) => {
+    res.json({api: 'up'})
+})
+
+router.use((err,req,res,next) => { //eslint-disable-line 
+    res.status(500).json({
+        customMessage: 'something went wrong inside the recipes', 
+        message: err.message, 
+        stack: err.stack,
+    })
+})
+
+
+module.exports = router 
+
---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 9000
+
+server.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
+});
